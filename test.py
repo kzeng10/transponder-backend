a5073dc83723df70d8db19e8da95b44e6d00c014
Maria Chavez <materechm@gmail.com>
2015-01-24 15:16:09 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import time
+from twilio.rest import TwilioRestClient
+
+account_sid = "ACa4d40581688ac7d51dc5c16b3c7e2137"
+auth_token  = "c8538afb552415cf2fa302df659d91c3"
+client = TwilioRestClient(account_sid, auth_token)
+
+
+run = raw_input("Start? > ")
+mins = 0
+if run == "start":
+    while mins != 15:
+        time.sleep(60)
+        mins += 1
+        if #user takes action on push notification:
+            break
+        else:
+            message = client.messages.create(body="test",
+                to="+14155496801",    # Replace with your phone number
+                from_="+19099627422") # Replace with your Twilio number
+            print message.sid
