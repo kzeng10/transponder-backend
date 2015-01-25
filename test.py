b32209cc0aa216e02051517cb219b589173faa75
materechm <materechm@gmail.com>
2015-01-24 16:14:02 -0800

    added timer

--- test.py
+++ test.py
@@ -6,10 +6,12 @@ account_sid = "ACa4d40581688ac7d51dc5c16b3c7e2137"
 auth_token  = "c8538afb552415cf2fa302df659d91c3"
 client = TwilioRestClient(account_sid, auth_token)
 
-
-run = raw_input("Start? > ")
-mins = 0
-if run == "start": #change this to when trip ends
+run = 0
+mins = raw_input("Mins?") #change this to whatever sets the time of the trip
+while mins != 0:
+    time.sleep(60)
+    mins -= 1
+if mins == 0:
     token_hex = '*******'
     payload = Payload(alert="Hello! Please check-in", sound="default", badge=1)
     apns.gateway_server.send_notification(token_hex, payload)
