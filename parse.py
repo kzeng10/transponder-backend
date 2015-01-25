86fed0750c45c156f54ccc03276cca0a8a81f421
kzeng10 <kzeng10@gmail.com>
2015-01-24 18:48:50 -0800

    working python parse script

---
+++ parse.py
@@ -0,0 +1,8 @@
+from parse_rest.connection import register
+from parse_rest.datatypes import Object
+register("lkHCX43bL8tqi0kpiICrOdXLlcx6yxDs3k9rUE5A", "uIE9zOF0dsbr5N9uPrtF2eBDiuGiIhuffvFsdaaA", master_key="ikropQhezf8HiYJKg0ZidOJ0Oa2I9RLRsdzP16Zt")
+class EmergencyContacts(Object):
+	pass
+
+contact = EmergencyContacts(Name="david", Email="david@gmail.com")
+contact.save()
