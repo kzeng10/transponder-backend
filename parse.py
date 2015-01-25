b967619614f8517c4424e6e42346934b8d1db2d5
kzeng10 <kzeng10@gmail.com>
2015-01-25 01:32:24 -0800

    update to work with Users class
    Since the Parse DB was changed to just one table called Users, I changed the code to reflect these changes. Seems to make the entire thing work a lot more easily too!

--- parse.py
+++ parse.py
@@ -9,50 +9,43 @@ account_sid = "ACa4d40581688ac7d51dc5c16b3c7e2137" #twilio
 auth_token  = "c8538afb552415cf2fa302df659d91c3" #twilio
 client = TwilioRestClient(account_sid, auth_token) #twilio
 
-class EmergencyContacts(Object):
-	#stores name of user's name and list of emergency phone numbers
-	pass
-
-class Confirmations(Object):
-	#stores time sent, whether it is confirmed, gps coords, and interval (yes, redundant data, but how else do we store this value?)
-	self.timeSent = datetime.datetime.now()
-	self.isConfirmed = False
-	self.reached = False
-	self.user = "" 		#user's id, so we know who it is that just left
-	self.interval = 60 	#number of minutes, user-configurable
-	pass
-
-#NOTE: ONLY IOS APP WILL BE ADDING ENTRIES TO EmergencyContacts AND Confirmations TABLES. WE ARE RETRIEVING DATA FROM PARSE.
-
-#during setup, user inputs entry into EmergencyContacts with name= user's name and phonenumbers = list of emergency contacts
-allEmergencyContacts = EmergencyContacts.Query.get(name=name).phoneNumbers #username is name of the iphone owner
-lastConfirmation = Confirmations.Query.all().order_by("-createdAt").limit(1).createdAt #spits out Date of the most recent entry
-while lastConfirmation != Confirmations.Query.all().order_by("-createdAt").limit(1).createdAt:
-	#this is ridiculous lol
-	#basically here it's continuously checking the most recent entry every 5 minutes
-	time.sleep(300) 		#so we don't overload the db
-	pass
+class Users(Object):
+	self.name = "" 			#user's name
+	self.contacts = "" 		#contacts.split(',') to convert to list. storing all emergency contacts here. looks like only phone numbers are necessary?
+	#self.emails = [] 		#maybe we can do something like this?
+	self.lastPing = 99 		#unix time, seconds
+	self.lastResponse = 99 	#unix time, seconds
+	self.latitude = 30
+	self.longitude = 30
+	self.onTrip = False
+	self.pingInterval = 30 	#in MINUTES
 
 
-#####################
-#we reach here when we see that someone is leaving, i.e. we have a new most recent entry
+#NOTE: ONLY IOS APP WILL BE ADDING/EDITTING THE Users TABLE. WE ARE RETRIEVING DATA FROM PARSE.
+cur_time = time.now() #in unix time
 while True:
-	#calls this as often as indicated by Confirmations.interval (refer to time.sleep below) to check if the person is okay
-	lastConfirmation = Confirmations.Query.all().order_by("-createdAt").limit(1).createdAt #spits out Date of the most recent entry
-	time.sleep(lastConfirmation.interval * 60)
-	mostRecent = Confirmations.Query.all().order_by("-createdAt").limit(1) #spits out Date of the most recent entry
-	if not mostRecent.isConfirmed: 	#if user is in emergency
-		for phone in allEmergencyContacts:
-			message = client.messages.create(body= str(user.name) +  "did not check in, person was last at" + str(user.location),
-				to= "+1"+ phone,    # Replace with your phone number
-				from_="+19099627422")
-		pass
-	elif mostRecent.reached:
-		system.exit(0)
-	else:
-		token_hex = '2ae38011b9d95323b090a0905dbce4f0b61341b0409892343b21330e976572c8'
-	    payload = Payload(alert="Hello! Please check-in", sound="default", badge=1)
-	    apns.gateway_server.send_notification(token_hex, payload)
-		pass
+	time.sleep(60) #so we don't overload the db
+	users = Users.Query.all()
+	for user in users:
+		if user.onTrip: 	#skips users who aren't on a trip
+			cur_time = time.now()
 
-#run a checking script in another file?
+			#check if response was late, i.e. time is up and last response was before last ping
+			#if yes, send twilio stuff
+			#else if now is ping + pinginterval*60, send another ping, update values
+
+			if cur_time >= user.lastPing + user_pintInterval*60 and user.lastResponse < user.lastPing:
+				#user hasn't responded to last ping, i.e. in an emergency
+				nums = users.contacts.split(',')
+				for num in nums:
+					#twilio code for sending emergency msg to all emergency contacts. include info about gps and current time
+					message = client.messages.create(body= "%s did not check in, person was last at GPS Coordinates [%.3f, %.3f]" % (user.name, user.latitude, user.longitude)
+													to= "+1"+ num,    # Replace with your phone number
+													from_="+19099627422") #twilio phone number
+			elif cur_time >= user.lastPing + user.pingInterval*60:
+				#update lastPing
+				user.lastPing = cur_time
+				#send push notification to iphone
+				token_hex = '2ae38011b9d95323b090a0905dbce4f0b61341b0409892343b21330e976572c8'
+				payload = Payload(alert="Hello! Please check-in", sound="default", badge=1)
+				apns.gateway_server.send_notification(token_hex, payload)
